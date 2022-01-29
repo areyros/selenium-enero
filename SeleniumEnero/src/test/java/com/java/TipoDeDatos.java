e4f9baff98490a0ae32d5321e91364861a823fc0
rera_ <rera_@LAPTOP-TLS3V1C2>
2022-01-29 11:41:35 -0600

    ADD chromedriver ADD Class DataTypes ADD Class Operadores ADD Class TipoDeDatos

---
+++ SeleniumEnero/src/test/java/com/java/TipoDeDatos.java
@@ -0,0 +1,22 @@
+package com.java;
+
+public class TipoDeDatos {
+
+	public static void main(String[] args) {
+		// Datos primitivos
+		
+		byte var1 = 1;
+		short var2 =1;
+		int var3 =1;
+		long var4 =1L;
+		float var5 = 1.2f;
+		double var6 = 1.2;
+		char var7 = 'a';
+		boolean var8 =true;
+		
+		String nombre = " Coral";
+	
+		System.out.println(nombre);
+	}
+
+}
